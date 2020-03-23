46ebb94a4804735febb5caaa941377f1ce52f67e
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-23 17:05:49 +0530

    Added:- to check for option 0 for no play 1 for snake 2 for ladder

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,6 +1,37 @@
-echo "welcome to snakeladder"
-startPosition=0
-firstPlayer=$startPosition
-function rollDie(){
-die=$((RANDOM%6 +1))
+echo "welcome to snakeladder" 
+startPosition=0 
+winningPosition=100 
+currentPosition=$startPosition 
+function rollDie()
+{ die=$((RANDOM%6 +1)) 
+checkOption
 }
+function checkOption(){
+noPlay=0
+snake=1
+ladder=2
+option=$((RANDOM%3))
+case $option in
+	$noplay)
+		currentPosition=$currentPosition
+		;;
+	$snake)	
+		((currentPosition-=$die))
+		if [ $currentPosition -lt $startPosition ]
+		then
+			currentPosition=$startPosition
+		fi
+		;;
+	$ladder)
+		 ((currentPosition+=$die))
+      if [ $currentPosition -gt $winningPosition ]
+      then
+         ((currentPosition-=$die))
+      fi
+      ;;
+
+esac
+}
+
+rollDie
+echo "$currentPosition"

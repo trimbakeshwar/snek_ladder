a16f5036a2324eab45e612714e392f7ca16eadcc
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-24 11:10:54 +0530

    Added:-play the game with two players

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,30 +1,33 @@
+
 #!/bin/bash -x
 echo "welcome to snakeladder" 
 startPosition=0 
 winningPosition=100 
 currentPosition=$startPosition
-numberOfTimeDieThrowForWin=0 
-function rollDie()
-{ die=$((RANDOM%6 +1)) 
-checkOption
+playerOne=$startPosition
+playerTwo=$startPosition
+
+function rollDie(){
+	die=$((RANDOM%6 +1)) 
+	checkOption
 }
 function checkOption(){
-noPlay=0
-snake=1
-ladder=2
-option=$((RANDOM%3))
-case $option in
-	$noplay)
-#			currentPosition=$currentPosition
+	noPlay=0
+	snake=1
+	ladder=2
+	option=$((RANDOM%3))
+	case $option in
+		$noplay)
+			currentPosition=$currentPosition
 			;;
-	$snake)	
+		$snake)	
 			((currentPosition-=$die))
 			if [ $currentPosition -lt $startPosition ]
          then
             currentPosition=$startPosition
          fi
 			;;
-	$ladder)
+		$ladder)
 			((currentPosition+=$die))
 			((numberOfTimeDieThrowForWin++))
 			if [ $currentPosition -gt $winningPosition ]
@@ -32,17 +35,39 @@ case $option in
             ((currentPosition-=$die))
          fi
       	;;
-esac
+	esac
 }
 
-count=0
-while [ $currentPosition -lt $winningPosition ]
-do
-   rollDie
-	echo "current position $currentPosition"
+function updatePosition(){
+	while [ $currentPosition -lt $winningPosition ]
+	do
+   	rollDie
+	done
+}
+function playerOne(){
+	numberOfTimeDieThrowForWin=0
+	updatePosition
+	playerOneCount=$numberOfTimeDieThrowForWin
+	echo $playerOneCount
+}
+function playerTwo(){
+	startPosition=0 
+	winningPosition=100 
+	currentPosition=$startPosition
+	numberOfTimeDieThrowForWin=0
+	updatePosition
+	playerTwoCount=$numberOfTimeDieThrowForWin
+echo $playerTwoCount
+}
 
-	((count++))
-done
-echo "number of time die thrown $count"
-echo " number of time die throw for only increase position :$numberOfTimeDieThrowForWin"
-echo "$currentPosition"
+result1="$( playerOne $(()) )"
+result2="$( playerTwo $(()) )"
+echo "player one : $result1"
+echo "player two : $result2"
+if [ $result1 -lt $result2 ]
+then
+   echo "player one win"
+else
+   echo "player two win"
+fi
+
